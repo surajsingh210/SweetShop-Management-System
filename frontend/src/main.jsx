0bd4f3953d4a97e37e579bdc0cac8ed99cb1eeb0
Suraj Singh <surajsingh95697@gmail.com>
2025-12-14 21:46:02 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,33 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import App from "./App.jsx";
-// import Login from "./pages/Login.jsx";
-// import Register from "./pages/Register.jsx";
-// import Dashboard from "./pages/Dashboard.jsx";
-// import ProtectedRoute from "./components/ProtectedRoute.jsx";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<App />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route
-//           path="/dashboard"
-//           element={
-//             <ProtectedRoute>
-//               <Dashboard />
-//             </ProtectedRoute>
-//           }
-//         />
-//       </Routes>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
