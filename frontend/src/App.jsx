3993dfd32c102f6aece939ef5756534d73ec0390
Suraj Singh <surajsingh95697@gmail.com>
2025-12-14 21:45:33 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,3 @@
-// import Navbar from "./components/Navbar";
-
-// export default function App() {
-//   return (
-//     <div>
-//       <Navbar />
-//       <div className="min-h-screen flex flex-col items-center justify-center bg-pink-50">
-//         <h1 className="text-4xl font-bold text-pink-600">
-//           Welcome to Sweet Shop 🎉
-//         </h1>
-//         <p className="text-gray-700 mt-4 text-lg">
-//           Manage, purchase, and restock sweets with ease.
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
-
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
